b95283147ab1ccb1749d8816ac0be0e0eea95b8e
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-07-10 16:11:27 +0700

    Add chalk to global functio

--- global.js
+++ global.js
@@ -1,12 +1,40 @@
+var chalk = require('chalk');
 var config = require('./config.js');
 
-function log(text){
-    if(config.debugMode){
+function log(text) {
+    if (config.debugMode) {
         console.log(text);
     }
 }
 
+function green(text) {
+    if (config.debugMode) {
+        console.log(chalk.green(text));
+    }
+}
+
+function cyan(text) {
+    if(config.debugMode){
+        console.log(chalk.cyan(text));
+    }
+}
+
+function red(text) {
+    if(config.debugMode){
+        console.log(chalk.red(text));
+    }
+}
+
+function error(text) {
+    if (config.debugMode) {
+        console.error(text);
+    }
+}
 
 module.exports = {
-    log: log
+    log: log,
+    green: green,
+    cyan: cyan,
+    red: red,
+    error: error
 }
